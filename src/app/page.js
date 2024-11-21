d58ee9667b7d0397733aee98a2fce5153bef3c4b
Rakibulislam-emon <rakibulislamemon60>
2024-11-22 02:58:48 +0600

    test

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>test..........hello</div>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, nesciunt facilis nam consectetur iusto quia, culpa doloremque aliquid quaerat unde commodi obcaecati consequatur vero animi suscipit tempore natus deleniti atque?</div>
   )
 }
